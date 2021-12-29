bc3aa4a0fcc13aad66d64ad327c6157f05cbf0c6
Oscar Linderholm <osm@recv.se>
2021-12-29 16:29:13 +0100

    Remove unused code

--- postgres/helper.go
+++ postgres/helper.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-
-	"github.com/osm/go-stack/tools"
 )
 
 func toSnakeCase(camel string) string {
@@ -48,48 +46,6 @@ func getPaginationQuery(query string, after *string, params *[]interface{}) (str
 	*params = append(*params, *cursorID)
 
 	return query, nil
-}
-
-func getInsertQuery(query string, input map[string]interface{}) string {
-	columns := `(`
-	values := "VALUES("
-	for k := range input {
-		columns = fmt.Sprintf(`%s"%s", `, columns, toSnakeCase(k))
-		values = fmt.Sprintf(`%s:%s, `, values, k)
-	}
-	columns = columns[:len(columns)-2] + ")"
-	values = values[:len(values)-2] + ")"
-	return fmt.Sprintf("%s %s %s", query, columns, values)
-}
-
-func getInsertQueryNew(query string, keys []string) string {
-	columns := `(`
-	values := "VALUES("
-
-	for idx, k := range keys {
-		columns = fmt.Sprintf(`%s"%s", `, columns, toSnakeCase(k))
-		values = fmt.Sprintf(`%s$%d, `, values, idx+1)
-	}
-
-	columns = columns[:len(columns)-2] + ")"
-	values = values[:len(values)-2] + ")"
-	return fmt.Sprintf("%s %s %s", query, columns, values)
-}
-
-func getUpdateQuery(query string, patch map[string]interface{}) string {
-	set := "SET "
-
-	patch["updated_at"] = tools.GetTimestamp()
-
-	for k := range patch {
-		if k == "id" {
-			continue
-		}
-
-		set = fmt.Sprintf(`%s"%s" = :%s, `, set, toSnakeCase(k), k)
-	}
-
-	return strings.Replace(query, "SET", set[0:len(set)-2], 1)
 }
 
 func getSortedKeys(data map[string]interface{}) []string {
